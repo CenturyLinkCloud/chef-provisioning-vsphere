95e6203dfa765b9948f09853410d634d70804098
mwrock <matt@mattwrock.com>
2014-06-11 14:55:59 -0700

    fixes to with_driver but untested

--- lib/chef_metal_vsphere.rb
+++ lib/chef_metal_vsphere.rb
@@ -5,8 +5,8 @@ class Chef
   module DSL
     module Recipe
       def with_vsphere_driver(driver_options = nil, &block)
-      	url = VsphereDriver.canonicalize_url({:driver_options => driver_options})
-  		with_driver url, driver_options, &block
+      	url, config = VsphereDriver.canonicalize_url(nil, {:driver_options => driver_options})
+  		with_driver url, config, &block
       end
     end
   end
