d31e29c03600409704e5aa7e1848833e0f993ce6
mwrock <matt@mattwrock.com>
2014-10-17 00:56:11 -0700

    conforming to latest test-kitchen windows work

--- lib/chef_metal_vsphere/version.rb
+++ lib/chef_metal_vsphere/version.rb
@@ -1,3 +1,3 @@
 module ChefMetalVsphere
-  VERSION = '0.3.53'
+  VERSION = '0.3.54'
 end
--- lib/kitchen/driver/vsphere.rb
+++ lib/kitchen/driver/vsphere.rb
@@ -1,8 +1,10 @@
-require 'kitchen/driver/vsphere_common'
+require 'kitchen'
+require 'chef_metal_vsphere/vsphere_driver'
+require 'chef_metal/chef_machine_spec'
 
 module Kitchen
   module Driver
-    class Vsphere < Kitchen::Driver::SSHBase
+    class Vsphere < Kitchen::Driver::Base
 
       default_config :machine_options,
         :start_timeout => 600,
@@ -21,7 +23,44 @@ module Kitchen
           }
         }
 
-      include Kitchen::Driver::VsphereCommon
+      def create(state)
+        config[:server_name] ||= "kitchen-#{SecureRandom.hex}-#{instance.name}"
+        state[:username] = config[:machine_options][:bootstrap_options][:ssh][:user]
+        state[:password] = config[:machine_options][:bootstrap_options][:ssh][:password]
+        
+        machine = with_metal_driver(config[:server_name]) do | action_handler, driver, machine_spec|
+          driver.allocate_machine(action_handler, machine_spec, config[:machine_options])
+          driver.ready_machine(action_handler, machine_spec, config[:machine_options])
+          state[:server_id] = machine_spec.location['server_id']
+          state[:hostname] = machine_spec.location['ipaddress']
+          state[:vsphere_name] = config[:server_name]
+        end
+      end
+
+      def destroy(state)
+        return if state[:server_id].nil?
+
+        with_metal_driver(state[:vsphere_name]) do | action_handler, driver, machine_spec|
+          machine_spec.location = { 'driver_url' => driver.driver_url,
+                        'server_id' => state[:server_id]}
+          driver.destroy_machine(action_handler, machine_spec, config[:machine_options])
+        end
+
+        state.delete(:server_id)
+        state.delete(:hostname)
+        state.delete(:vsphere_name)
+      end
+
+      def with_metal_driver(name, &block)
+        Cheffish.honor_local_mode do
+          chef_server = Cheffish.default_chef_server(Cheffish.profiled_config)
+          config[:machine_options][:convergence_options] = {:chef_server => chef_server}
+          machine_spec = ChefMetal::ChefMachineSpec.new({'name' => name}, chef_server)
+          driver = ChefMetal.driver_for_url("vsphere://#{config[:driver_options][:host]}", config)
+          action_handler = ChefMetal::ActionHandler.new
+          block.call(action_handler, driver, machine_spec)
+        end
+      end
 
     end
   end
